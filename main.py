b23d17644ab058ca342a6b73809fb2ce081bf241
Lindo-Mbatha <www.lindo101@gmail.com>
2021-02-14 17:42:36 +0200

    Odds or even

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Odds or Even")
+
+number = int(input("Which number do you want to check? "))
+
+result = number % 2
+
+if result == 0:
+  print("This is an even number")
+else:
+  print("This is an odd number")
